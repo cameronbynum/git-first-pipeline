b63e15090f8bae27df6d0bfca0a261cffbc5bc60
Cameron Bynum <bynum.cameron@gmail.com>
2021-06-02 17:44:19 -0400

    figuring out how pathing works.. >.<

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname+'/public'));
 //   res.send("Refresh page for new number: " + ran_num.between(0,1000))
 // })
 
-app.get('/index.html', (req, res) => {
+app.get('/index', (req, res) => {
   fs.readFile('./public/html/index.html', function(error, content) {
     if (error) {
         res.writeHead(500);
@@ -22,7 +22,20 @@ app.get('/index.html', (req, res) => {
   });
 })
 
-app.get('/about-me.html', (req, res) => {
+app.get('/', (req, res) => {
+  fs.readFile('./public/html/index.html', function(error, content) {
+    if (error) {
+        res.writeHead(500);
+        res.end();
+    }
+    else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content, 'utf-8');
+    }
+  });
+})
+
+app.get('/about-me', (req, res) => {
   fs.readFile('./public/html/about-me.html', function(error, content) {
     if (error) {
         res.writeHead(500);
@@ -35,7 +48,7 @@ app.get('/about-me.html', (req, res) => {
   });
 })
 
-app.get('/pathing-visualizer.html', (req, res) => {
+app.get('/pathing-visualizer', (req, res) => {
   fs.readFile('./public/html/pathing-visualizer.html', function(error, content) {
     if (error) {
         res.writeHead(500);
