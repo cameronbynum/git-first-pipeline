5b7423189c0da4cc46758c69f6ac3dbaf8a6202e
Cameron Bynum <bynum.cameron@gmail.com>
2021-06-02 15:31:19 -0400

    add random num gen

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-
-
-
 const express = require('express');
 const app = express();
-
 const port = process.env.port || 8080
+var ran_num = require('./javascript/random_number_generator')
+
 
 app.get('/', (req, res) => {
-  res.send("Welcome to the homepage")
+  res.send("Refresh page for new number: " + ran_num.between(0,1000))
 })
 
 app.listen(port, () =>{
   console.log("new node js works")
 })
+
