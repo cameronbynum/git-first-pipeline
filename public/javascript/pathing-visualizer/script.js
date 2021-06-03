38bc0bddc2be82fbae309ecc7ecbaf342e2b2b39
Cameron Bynum <bynum.cameron@gmail.com>
2021-06-03 18:14:18 -0400

    adjustable grid size

--- public/javascript/pathing-visualizer/script.js
+++ public/javascript/pathing-visualizer/script.js
@@ -3,10 +3,27 @@ script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
-const container = document.getElementById("container");
+
 
 //function makeRows originated from https://stackoverflow.com/questions/57550082/creating-a-16x16-grid-using-javascript
-function makeRows(rows, cols) {
+function makeRows(rows, cols, element) {
+  var container = document.getElementById(element)
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  //container.style.setProperty('width', cols*3)
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    cell.innerText = (c + 1);
+    // cell.style.setProperty('height', cell.style.getPropertyValue('width'));
+    cell.style.setProperty('height', 'auto');
+    cell.style.setProperty('width', 'auto');
+    container.appendChild(cell).className = "grid-item";
+  }
+  onClickGridItems();
+
+};
+
+function makeRows2(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
   container.style.setProperty('width', cols*3)
@@ -16,6 +33,7 @@ function makeRows(rows, cols) {
     // cell.style.setProperty('height', cell.style.getPropertyValue('width'));
     cell.style.setProperty('height', 'auto');
     cell.style.setProperty('width', 'auto');
+    cell.style.setProperty('background-color', 'red');
     container.appendChild(cell).className = "grid-item";
   }
   onClickGridItems();
@@ -39,10 +57,6 @@ function onClickGridItems(){
       gridItem.addEventListener("mouseout",function(){
         this.style.setProperty('background-color', 'white')
       })
-      
-      // gridItem.onmouseover = function() {
-      //   gridItem.style.setProperty('background-color', 'red')
-      // }
     }
   }
   
@@ -54,8 +68,27 @@ function onClickGridItems(){
   }
 }
 
+function removeGridItems(){
+  gridItems = document.getElementsByClassName('grid-item')
+  for(var i = gridItems.length-1; i>=0; i--){
+    var gridItem = gridItems[i]
+    console.log('hello')
+    gridItem.remove()
+  }
+}
 
+function createNewGrid(){
+  removeGridItems()
+  rows = document.getElementById("rows").value
+  cols = document.getElementById("cols").value
+  console.log(rows + cols)
+  makeRows(rows, cols, "container")
+}
 
+makeRows(rows = document.getElementById("rows").value, document.getElementById("cols").value, "container");
 
-makeRows(32, 32);
+// container.remove()
+// test = document.createElement("div");
+// test.id = "container"
+// makeRows(32, 32, "container");
 
