344f1014df2349ec1cf3182311dc3712841b3922
Cameron Bynum <bynum.cameron@gmail.com>
2021-06-08 21:17:54 -0400

    almost have algo working

--- public/javascript/pathing-visualizer/dijkstra.js
+++ public/javascript/pathing-visualizer/dijkstra.js
@@ -2,56 +2,87 @@
 // in which they were visited. Also makes nodes point back to their
 // previous node, effectively allowing us to compute the shortest path
 // by backtracking from the finish node.
-export function dijkstra(grid, startNode, finishNode) {
+export function dijkstra(startNode, finishNode) {
   const visitedNodesInOrder = [];
-  startNode.distance = 0;
-  const unvisitedNodes = getAllNodes(grid);
+
+  startNode.dataset.distance = Infinity;
+  
+
+  //const unvisitedNodes = getAllNodes(grid);
+  const unvisitedNodes = [].slice.call(document.getElementsByClassName('grid-item'));
+  const allNodes = [].slice.call(document.getElementsByClassName('grid-item'));
+  //console.log((unvisitedNodes))
+
   while (!!unvisitedNodes.length) {
     sortNodesByDistance(unvisitedNodes);
     const closestNode = unvisitedNodes.shift();
+    //console.log(closestNode)
     // If we encounter a wall, we skip it.
     if (closestNode.isWall) continue;
     // If the closest node is at a distance of infinity,
     // we must be trapped and should therefore stop.
-    if (closestNode.distance === Infinity) return visitedNodesInOrder;
-    closestNode.isVisited = true;
+    if (closestNode.dataset.distance === Infinity) return visitedNodesInOrder;
+    closestNode.dataset.isVisited = true;
     visitedNodesInOrder.push(closestNode);
     if (closestNode === finishNode) return visitedNodesInOrder;
-    updateUnvisitedNeighbors(closestNode, grid);
+    updateUnvisitedNeighbors(closestNode, allNodes);
   }
 }
 
 function sortNodesByDistance(unvisitedNodes) {
-  unvisitedNodes.sort((nodeA, nodeB) => nodeA.distance - nodeB.distance);
+  Array.prototype.slice(unvisitedNodes).sort((nodeA, nodeB) => nodeA.dataset.distance - nodeB.dataset.distance);
 }
 
-function updateUnvisitedNeighbors(node, grid) {
-  const unvisitedNeighbors = getUnvisitedNeighbors(node, grid);
+function updateUnvisitedNeighbors(node, allNodes) {
+  const unvisitedNeighbors = getUnvisitedNeighbors(node, allNodes);
+  //console.log(unvisitedNeighbors)
   for (const neighbor of unvisitedNeighbors) {
-    neighbor.distance = node.distance + 1;
+    neighbor.dataset.distance = node.dataset.distance + 1;
+    console.log(node.dataset.distance + 1)
+    neighbor.style.setProperty('data-previousnode', node)
     neighbor.previousNode = node;
   }
 }
 
-function getUnvisitedNeighbors(node, grid) {
+function getUnvisitedNeighbors(node, allNodes) {
   const neighbors = [];
-  const {col, row} = node;
-  if (row > 0) neighbors.push(grid[row - 1][col]);
-  if (row < grid.length - 1) neighbors.push(grid[row + 1][col]);
-  if (col > 0) neighbors.push(grid[row][col - 1]);
-  if (col < grid[0].length - 1) neighbors.push(grid[row][col + 1]);
-  return neighbors.filter(neighbor => !neighbor.isVisited);
+  const col = parseInt(node.dataset.col)
+  const row = parseInt(node.dataset.row)
+  const maxRows = parseInt(document.getElementById("rows").value) - 1
+  const maxCols = parseInt(document.getElementById("cols").value) - 1
+  console.log(row)
+  console.log(col)
+  //if (row > 0) neighbors.push(allNodes[row - 1][col]);
+  //if row is zero then no neighbor above
+  if (row > 0) neighbors.push(allNodes[parseInt((row*maxCols + col))])
+  //if (row < allNodes.length - 1) neighbors.push(allNodes[row + 1][col]);
+  //if row is last row then no neighbor beneath
+  if (row < maxRows - 1) neighbors.push(allNodes[parseInt((row*maxCols + col) + maxCols + 1)])
+  //if (col > 0) neighbors.push(allNodes[row][col - 1]);
+  //if col is zero then no neighbor above
+  if (col > 0) neighbors.push(allNodes[parseInt((row*maxCols + col)+1)])
+  if (col < allNodes[0].length - 1) neighbors.push(allNodes[(row*document.getElementById("cols").value + col)+1]);
+  //if col is last col then no neighbor beneath
+  // console.log("col: " + col)
+  // console.log("Max Rows: " + maxRows)
+  // console.log("row: " + row)
+  const num =((col*maxRows) + row) + 1
+
+  console.log(num)
+  if (col < maxCols - 1) neighbors.push(allNodes[parseInt((col*maxRows + row)+1)])
+  console.log(neighbors)
+  return neighbors.filter(neighbor => !neighbor.dataset.isVisited);
 }
 
-function getAllNodes(grid) {
-  const nodes = [];
-  for (const row of grid) {
-    for (const node of row) {
-      nodes.push(node);
-    }
-  }
-  return nodes;
-}
+// function getAllNodes(grid) {
+//   const nodes = [];
+//   for (const row of grid) {
+//     for (const node of row) {
+//       nodes.push(node);
+//     }
+//   }
+//   return nodes;
+// }
 
 // Backtracks from the finishNode to find the shortest path.
 // Only works when called *after* the dijkstra method above.
@@ -59,8 +90,9 @@ export function getNodesInShortestPathOrder(finishNode) {
   const nodesInShortestPathOrder = [];
   let currentNode = finishNode;
   while (currentNode !== null) {
+    //console.log(currentNode)
     nodesInShortestPathOrder.unshift(currentNode);
-    currentNode = currentNode.previousNode;
+    currentNode = currentNode.dataset.previousNode;
   }
   return nodesInShortestPathOrder;
 }
--- public/javascript/pathing-visualizer/script.js
+++ public/javascript/pathing-visualizer/script.js
@@ -14,10 +14,31 @@ function makeRows(rows, cols, element) {
   container.style.setProperty('--grid-rows', rows)
   container.style.setProperty('--grid-cols', cols)
 
-  for(let i = 1; i <= rows; i++){
-    console.log("howdy")
-    for(let j = 1; j <= cols; j++){
+  for(let i = 0; i < rows; i++){
+    for(let j = 0; j < cols; j++){
+
       let node = createNode(i, j, (i*cols)+j)
+      if(i == 0 && j == 0){
+        if(node.getAttribute("data-value")!='wall'){
+          removeExtraNodes('starting-point')
+          node.setAttribute('data-value', 'starting-point')
+          node.style.setProperty('background-color', 'red')
+          node.addEventListener("mouseout",function(){
+            node.style.setProperty('background-color', 'red')
+          })
+        }
+      }
+      if(i == 9 && j == 9){
+        if(node.getAttribute("data-value")!='wall'){
+          removeExtraNodes('ending-point')
+          node.setAttribute('data-value', 'ending-point')
+          node.style.setProperty('background-color', 'black')
+          node.addEventListener("mouseout",function(){
+            node.style.setProperty('background-color', 'black')
+          })
+        }
+      }
+      
       container.appendChild(node).className = "grid-item";
     }
   }
@@ -38,22 +59,21 @@ function makeRows(rows, cols, element) {
 
 };
 
-function visualizeDijkstra() {
-  const {grid} = this.state;
-  const startNode = grid[START_NODE_ROW][START_NODE_COL];
-  const finishNode = grid[FINISH_NODE_ROW][FINISH_NODE_COL];
-  const visitedNodesInOrder = dijkstra(grid, startNode, finishNode);
+ window.visualizeDijkstra = function() {
+  const startNode = getNodeByName('starting-point')
+  const finishNode = getNodeByName('ending-point')
+  const visitedNodesInOrder = dijkstra(startNode, finishNode);
+  //console.log(visitedNodesInOrder)
   const nodesInShortestPathOrder = getNodesInShortestPathOrder(finishNode);
   this.animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder);
 }
 
-function getStartingNode(){
+function getNodeByName(name){
   var gridItems = document.getElementsByClassName('grid-item');
-
   for(let i = 0; i < gridItems.length; i++) {
     var gridItem = gridItems[i];
-    if(gridItem.getAttribute("data-value")=='starting-node'){
-      return(gridItem.getAttribute)
+    if(gridItem.getAttribute("data-value")==name){
+      return(gridItem)
     }
     
     
@@ -68,6 +88,8 @@ function createNode(row, col, num){
   node.setAttribute('data-col', col)
   node.setAttribute('data-isVisited', false)
   node.setAttribute('data-num', num)
+  node.setAttribute('data-distance', Infinity)
+  node.innerHTML = num
   //node.id = ('grid-item: ' + (c+1))
   node.addEventListener("mouseout",function(){
     this.style.setProperty('background-color', 'white')
@@ -76,12 +98,12 @@ function createNode(row, col, num){
   return node;
 }
 
-function removeExtraPoints(pointName){
+function removeExtraNodes(nodeName){
   var gridItems = document.getElementsByClassName('grid-item');
 
   for(let i = 0; i < gridItems.length; i++) {
     var gridItem = gridItems[i];
-    if(gridItem.getAttribute("data-value")==pointName){
+    if(gridItem.getAttribute("data-value")==nodeName){
       gridItem.removeAttribute("data-value")
       gridItem.style.setProperty('background-color', 'white')
       gridItem.addEventListener("mouseout",function(){
@@ -94,7 +116,7 @@ function removeExtraPoints(pointName){
   }
 }
 
-function startingPointGridItems(){
+ window.startingPointGridItems = function(){
   var gridItems = document.getElementsByClassName('grid-item');
 
   for(var i = 0; i < gridItems.length; i++) {
@@ -104,7 +126,7 @@ function startingPointGridItems(){
     })
     gridItem.onclick = function() {
       if(this.getAttribute("data-value")!='wall'){
-        removeExtraPoints('starting-point')
+        removeExtraNodes('starting-point')
         this.setAttribute('data-value', 'starting-point')
         this.addEventListener("mouseout",function(){
           this.style.setProperty('background-color', 'red')
@@ -114,7 +136,7 @@ function startingPointGridItems(){
   }
 }
 
-function endingPointGridItems(){
+window.endingPointGridItems = function(){
   var gridItems = document.getElementsByClassName('grid-item');
 
   for(var i = 0; i < gridItems.length; i++) {
@@ -124,7 +146,7 @@ function endingPointGridItems(){
     })
     gridItem.onclick = function() {
       if(this.getAttribute("data-value")!='wall'){
-        removeExtraPoints('ending-point')
+        removeExtraNodes('ending-point')
         this.setAttribute('data-value', 'ending-point')
         this.addEventListener("mouseout",function(){
           this.style.setProperty('background-color', 'black')
@@ -134,7 +156,7 @@ function endingPointGridItems(){
   }
 }
 
-function wallGridItems(){
+ window.wallGridItems = function(){
   var gridItems = document.getElementsByClassName('grid-item');
 
   for(var i = 0; i < gridItems.length; i++) {
@@ -153,25 +175,17 @@ function wallGridItems(){
 }
 
 function removeGridItems(){
-  gridItems = document.getElementsByClassName('grid-item')
+  var gridItems = document.getElementsByClassName('grid-item')
   for(var i = gridItems.length-1; i>=0; i--){
     var gridItem = gridItems[i]
     gridItem.remove()
   }
 }
-
-function createNewGrid(){
+  window.createNewGrid = function(){
   removeGridItems()
-  rows = document.getElementById("rows").value
-  cols = document.getElementById("cols").value
+  var rows = document.getElementById("rows").value
+  var cols = document.getElementById("cols").value
   makeRows(rows, cols, "container")
 }
 
-console.log(document.getElementById("rows").value)
 makeRows(document.getElementById("rows").value, document.getElementById("cols").value, "container");
-
-// container.remove()
-// test = document.createElement("div");
-// test.id = "container"
-// makeRows(32, 32, "container");
-
